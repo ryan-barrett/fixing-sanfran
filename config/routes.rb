c7a9e3ed179535cde828ac7e50e48e38a0dcee38
ryan-barrett <ryan.barrett@spookyhouse.org>
2017-09-12 22:12:08 -0700

    created post model and added signup / login pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   #Home route
   get 'home/index', as: 'home'
+
+  #User routes
+  get '/users/new', to: 'users#new', as: 'new_user'
+  get '/users/:id', to: 'users#show', as: 'user'
+  post '/users', to: 'users#create'
+
+  #Session routes
+  get '/login', to: 'sessions#new'
+   get '/logout', to: 'sessions#destroy'
+  post '/sessions', to: 'sessions#create'
+
 end
