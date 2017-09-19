8f9af15d4d90e7ce687b964a530cab17f3d106c9
ryan-barrett <ryan.barrett@spookyhouse.org>
2017-09-19 09:57:32 -0700

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-first_user = User.create(first_name: 'me', last_name: 'you', password: 123, user_name: "hello")
+# first_user = User.create(first_name: 'me', last_name: 'you', password: 123, user_name: "hello")
 first_post = Post.create(title: "test", description:"yay")
